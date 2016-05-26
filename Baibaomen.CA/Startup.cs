9360ec94557efe8f9b3ba580176c10f2add2e875
Jack Wang <wangzhifeng.yes@gmail.com>
2016-05-26 17:05:52 +0800

    Demo CA setup.

---
+++ Baibaomen.CA/Startup.cs
@@ -0,0 +1,26 @@
+﻿using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Services.InMemory;
+using Owin;
+using System;
+using System.Collections.Generic;
+
+namespace Baibaomen.CA
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var options = new IdentityServerOptions
+            {
+                Factory = new IdentityServerServiceFactory()
+                            .UseInMemoryClients(ClientsService.Get())
+                            .UseInMemoryScopes(ScopesService.Get())
+                            .UseInMemoryUsers(new List<InMemoryUser>()),
+
+                RequireSsl = false
+            };
+
+            app.UseIdentityServer(options);
+        }
+    }
+}
