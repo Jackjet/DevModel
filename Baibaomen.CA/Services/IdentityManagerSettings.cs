30dd17a5831b3a54908e3d621804bb48de325630
Jack Wang <wangzhifeng.yes@gmail.com>
2016-06-01 15:52:23 +0800

    添加CA对ASPNET Identity的支持；添加CA的用户管理界面。

---
+++ Baibaomen.CA/Services/IdentityManagerSettings.cs
@@ -0,0 +1,50 @@
+﻿using IdentityManager;
+using IdentityManager.AspNetIdentity;
+using IdentityManager.Configuration;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Baibaomen.CA
+{
+    public class IdentityManagerService<TUser, TUserKey, TRole, TRoleKey> : AspNetIdentityManagerService<TUser, TUserKey, TRole, TRoleKey> where TUser : class, IUser<TUserKey>, new()
+        where TUserKey : IEquatable<TUserKey>
+        where TRole : class, IRole<TRoleKey>, new()
+        where TRoleKey : IEquatable<TRoleKey>
+    {
+        public IdentityManagerService(UserManager<TUser, TUserKey> userManager, RoleManager<TRole, TRoleKey> roleManager)
+            : base(userManager, roleManager)
+        {
+        }
+    }
+
+    public static class IdentityManagerServiceExtensions
+    {
+        public static IdentityManagerServiceFactory Configure(this IdentityManagerServiceFactory factory, string connectionString)
+        {
+            //factory.Register(new Registration<Context>(resolver => new Context(connectionString)));
+            //factory.Register(new Registration<UserStore>());
+            //factory.Register(new Registration<RoleStore>());
+            //factory.Register(new Registration<UserManager>());
+            //factory.Register(new Registration<RoleManager>());
+
+            //factory.IdentityManagerService = new Registration<IdentityManager.IIdentityManagerService, IdentityManagerService<IdentityUser,string,IdentityRole,string>>();
+
+            //return factory;
+            
+            factory.Register(new Registration<IdentityDbContext>());
+            factory.Register(new Registration<UserStore<IdentityUser>>());
+            factory.Register(new Registration<RoleStore<IdentityRole>>());
+            factory.Register(new Registration<UserManager<IdentityUser, string>>(x => new UserManager<IdentityUser>(x.Resolve<UserStore<IdentityUser>>())));
+            factory.Register(new Registration<RoleManager<IdentityRole, string>>(x => new RoleManager<IdentityRole>(x.Resolve<RoleStore<IdentityRole>>())));
+
+            factory.IdentityManagerService = new Registration<IIdentityManagerService, AspNetIdentityManagerService<IdentityUser, string, IdentityRole, string>>();
+
+            return factory;
+        }
+    }
+
+}
--- Baibaomen.CA/Startup.cs
+++ Baibaomen.CA/Startup.cs
@@ -1,29 +1,36 @@
-﻿using IdentityServer3.Core.Configuration;
+﻿using IdentityManager.Configuration;
+using IdentityServer3.Core.Configuration;
 using Owin;
 using System;
+using System.Configuration;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Baibaomen.CA
 {
     public class Startup
     {
+        static string conn = ConfigurationManager.AppSettings["DBConnection"];
+
         public void Configuration(IAppBuilder app)
         {
             var options = new IdentityServerOptions
             {
                 SiteName = "IdentityServer",
                 SigningCertificate = LoadCertificate(),
-                Factory = new IdentityServerServiceFactory()
-                            .UseInMemoryClients(ClientService.Get())
-                            .UseInMemoryScopes(ScopeService.Get())
-                            .UseInMemoryUsers(UserService.Get()),
+                //Factory = new IdentityServerServiceFactory()
+                //            .UseInMemoryClients(ClientService.Get())
+                //            .UseInMemoryScopes(ScopeService.Get())
+                //            .UseInMemoryUsers(UserService.Get()),
+                Factory = new IdentityServerServiceFactory().Configure(conn),
                 RequireSsl = true
             };
-
-            //app.Map("/identity", identity =>
-            //{
-            //    identity.UseIdentityServer(options);
-            //});
+            
+            app.Map("/admin", adminApp => {
+                adminApp.UseIdentityManager(new IdentityManagerOptions()
+                {
+                    Factory = new IdentityManagerServiceFactory().Configure(conn)
+                });
+            });
 
             app.UseIdentityServer(options);
         }
