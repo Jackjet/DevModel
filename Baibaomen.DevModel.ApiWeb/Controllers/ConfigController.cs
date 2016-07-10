1f0e1a27277f652c3ecd8c2f9b3b9a00bc4ea6df
BaiBaoMen <baibaomen@gmail.com>
2016-07-10 22:36:38 +0800

    Add stub for config controller.

---
+++ Baibaomen.DevModel.ApiWeb/Controllers/ConfigController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Baibaomen.DevModel.ApiWeb.Controllers
+{
+    /// <summary>
+    /// Get config for clients.
+    /// </summary>
+    [RoutePrefix("api/config")]
+    public class ConfigController : ApiController
+    {
+    }
+}
