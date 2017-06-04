bdc1fb400a702cd74c473b516b5cb4141e82415f
baibaomen <baibaomen@gmail.com>
2017-06-04 21:19:50 +0800

    添加Hero练习代码。 准备添加从URL读取参数的代码。

--- Baibaomen.DevModel.NgSite/src/app/app.module.ts
+++ Baibaomen.DevModel.NgSite/src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeroDetailComponent,
+        HeroesComponent,
+        DashboardComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot([{
+            path: 'heroes',
+            component: HeroesComponent
+        },
+        {
+            path: 'dashboard',
+            component: DashboardComponent
+            },
+        {
+            path: '',
+            redirectTo: '/dashboard',
+            pathMatch:'full'
+        }
+        ], { useHash:true })
+    ],
+    providers: [
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Baibaomen.DevModel.NgSite/src/app/hero.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './hero-mock';
+
+@Injectable()
+export class HeroService {
+    getHeroes(): Promise<Hero[]> {
+        //return Promise.resolve(HEROES);
+        return new Promise(resolve => setTimeout(() => resolve(HEROES), 500));
+    }
+}
